66e21bfd7b408f3776cfa7ee32114b744d358d5e
ayuukumakuma <ayuu.kumakuma@gmail.com>
2024-08-14 13:31:31 +0900

    setup.shの設置

--- zsh/sync/check-and-sync-brewfile.zsh
+++ zsh/sync/check-and-sync-brewfile.zsh
@@ -4,10 +4,10 @@ function check_and_sync_brewfile() {
   # 現在インストールされているパッケージ一覧を取得
   installed_packages=$(brew leaves && brew list --cask)
   # Brewfileに記載されているパッケージ一覧を取得
-  brewfile_packages=$(grep '^(brew|cask)' ~/dotfiles/homebrew/Brewfile | awk '{gsub(/"/, "", $2); print $2}')
+  brewfile_packages=$(grep '^(brew|cask)' ~/dotfiles/homebrew/.Brewfile | awk '{gsub(/"/, "", $2); print $2}')
 
-  # インストールされていないパッケージをインストールし、Brewfileに追加
-  echo "Brewfileに記載されているがインストールされていないパッケージをインストールします:"
+  # インストールされていないパッケージをインストールし、.Brewfileに追加
+  echo ".Brewfileに記載されているがインストールされていないパッケージをインストールします:"
 
   installed_any=false
   # すべてのパッケージを1行ずつ処理
@@ -26,21 +26,23 @@ function check_and_sync_brewfile() {
 
   echo ""
 
-  # Brewfileに記載されていないがインストールされているパッケージをBrewfileに追加
-  echo "Brewfileに記載されていないがインストールされているパッケージをBrewfileに追加します:"
+  # Brewfileに記載されていないがインストールされているパッケージを.Brewfileに追加
+  echo "Brewfileに記載されていないがインストールされているパッケージを.Brewfileに追加します:"
   installed_any=false
   # すべてのパッケージを1行ずつ処理
   while read -r package; do
     if ! echo "$brewfile_packages" | grep -q "^$package$"; then
-      echo "追加中: $package"
-      echo "brew \"$package\"" >>~/dotfiles/homebrew/Brewfile
       installed_any=true
     fi
   done < <(echo "$installed_packages")
 
   # 該当するパッケージがなかった場合のメッセージ
   if [ "$installed_any" = false ]; then
-    echo "すべてのパッケージがすでにBrewfileに記載されています。"
+    echo "すべてのパッケージがすでに.Brewfileに記載されています。"
+  else
+    echo ".Brewfileの内容と実際のインストール状況が一致しません。"
+    echo ".Brewfileを再生成します。"
+    brew bundle dump --force --file=~/dotfiles/homebrew/.Brewfile
   fi
 
 }
