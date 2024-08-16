1f09b2b7b3b3b34487428b45a9ac9a66b0782175
ayuukumakuma <ayuu.kumakuma@gmail.com>
2024-08-16 21:36:20 +0900

    chore: Update git path to Homebrew version

--- replace-git.sh
+++ replace-git.sh
@@ -14,20 +14,18 @@ GIT_PATH=$(brew --prefix)/bin/git
 # 現在のgitのパスを確認
 CURRENT_GIT_PATH=$(which git)
 
-# デフォルトのgitがすでにbrewのものかどうか確認
+# デフォルトのgitがbrewのものかどうか確認
 if [ "$CURRENT_GIT_PATH" == "$GIT_PATH" ]; then
     echo "すでにHomebrewでインストールされたgitがデフォルトになっています。"
 else
     echo "Homebrewでインストールされたgitをデフォルトに設定します..."
 
     # ~/.zshrc にパスを追加
-    SHELL_CONFIG_FILE="$HOME/.zshrc"
+    SHELL_PATH_FILE="$HOME/dotfiles/zsh/sync/path.zsh"
+    echo "export PATH=\"$GIT_PATH:\$PATH\"" >>"$SHELL_PATH_FILE"
 
-    # シェル設定ファイルにパスを追加
-    echo "export PATH=\"$GIT_PATH:\$PATH\"" >>"$SHELL_CONFIG_FILE"
-
-    # シェル設定を再読み込み
-    source "$SHELL_CONFIG_FILE"
+    # 再読み込み
+    source "$HOME"/.zshrc
 
     echo "Homebrewでインストールされたgitがデフォルトに設定されました。"
     echo "新しいターミナルセッションを開くと、設定が反映されます。"
