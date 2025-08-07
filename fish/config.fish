626325940438900faf94173d21a3c6509d30527b
ayuukumakuma <ayuu.kumakuma@gmail.com>
2025-08-07 21:50:01 +0900

    config.fishの改善

--- fish/config.fish
+++ fish/config.fish
@@ -1,14 +1,22 @@
-# brewでインストールしたfisherをnixpkgsでインストールしたfishで使うための設定
+# brewでインストールしたfisherをnixpkgsでインストールしたfishで使う
 if test -d /opt/homebrew/share/fish/vendor_functions.d
     set -p fish_function_path /opt/homebrew/share/fish/vendor_functions.d
 end
 
 if status is-interactive
   # Configurations for plugin: fish-autols
-  set -U autols_cmd ls -al
+  set -gx autols_cmd ls -al
 
   # Configurations for git
-  set -Ux GIT_CONFIG_GLOBAL ~/.config/git/config
+  set -gx GIT_CONFIG_GLOBAL ~/.config/git/config
+
+  # Environment variables for Ruby building with Nix packages
+  set -gx PKG_CONFIG_PATH "$HOME/.nix-profile/lib/pkgconfig"
+  set -gx CFLAGS "-I$HOME/.nix-profile/include"
+  set -gx LDFLAGS "-L$HOME/.nix-profile/lib"
+
+  # mise activate
+  ~/.nix-profile/bin/mise activate fish | source
 
   # Abbreviation
   abbr -a c 'clear'
@@ -17,11 +25,3 @@ if status is-interactive
   abbr -a g 'git'
   abbr -a pn 'pnpm'
 end
-
-# Environment variables for Ruby building with Nix packages
-set -gx PKG_CONFIG_PATH "$HOME/.nix-profile/lib/pkgconfig"
-set -gx CFLAGS "-I$HOME/.nix-profile/include"
-set -gx LDFLAGS "-L$HOME/.nix-profile/lib"
-
-# mise activate
-~/.nix-profile/bin/mise activate fish | source
