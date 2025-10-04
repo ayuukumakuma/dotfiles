20a39a07ae39213c769f76961ef6b93caeca8b4a
ayuukumakuma <ayuu.kumakuma@gmail.com>
2025-10-04 22:09:49 +0900

    weztermのカラースキームを'nord'に変更し、フォント設定を'HackGen35 Console NF'と'Noto Color Emoji'に更新。フォントサイズを14に設定し、SHIFT + Enterキーのアクションを追加。

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,12 +3,15 @@ local wezterm = require('wezterm')
 local config = wezterm.config_builder()
 
 -- Color Scheme
-config.color_scheme = 'Kanagawa (Gogh)'
+config.color_scheme = 'nord'
 
 -- Font Settings
-config.font = wezterm.font {
-  family = 'Google Sans Code',
+config.font = wezterm.font_with_fallback {
+  'HackGen35 Console NF',
+  'Noto Color Emoji',
 }
+config.font_size = 14
+
 config.line_height = 1.1
 
 -- Cursor Settings
@@ -25,5 +28,9 @@ config.macos_window_background_blur = 30
 -- Tab Bar Settings
 config.hide_tab_bar_if_only_one_tab = true
 config.show_tab_index_in_tab_bar = false
+
+config.keys = {
+  {key="Enter", mods="SHIFT", action=wezterm.action{SendString="\x1b\r"}},
+}
 
 return config
