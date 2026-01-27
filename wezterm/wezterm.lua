65328262fe3aba978e03aa37f9d1a1aad01d2980
ayuukumakuma <ayuu.kumakuma@gmail.com>
2026-01-27 14:11:11 +0900

    feat(zellij): zellij設定を追加
    - zellij設定ファイルとREADMEを追加
    - mise設定を更新
    - wezterm設定を更新

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -23,7 +23,7 @@ config.window_close_confirmation = 'NeverPrompt'
 
 -- Background Settings
 config.window_background_opacity = 0.8
-config.macos_window_background_blur = 30
+config.macos_window_background_blur = 0
 
 -- Tab Bar Settings
 config.hide_tab_bar_if_only_one_tab = true
---
+++ zellij/README.md
@@ -0,0 +1,4 @@
+## config.kdl
+```sh
+~/.config/zellij/config.kdl
+```
