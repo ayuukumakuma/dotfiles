6e3dc49198c3e735777e21eb10803a52f5e90acf
ayuukumakuma <ayuu.kumakuma@gmail.com>
2026-02-13 15:53:16 +0900

    chore(config): ローカルツール設定を更新

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -23,7 +23,7 @@ config.window_decorations = 'RESIZE'
 config.window_close_confirmation = 'NeverPrompt'
 
 -- Background Settings
-config.window_background_opacity = 0.8
+config.window_background_opacity = 0.7
 config.macos_window_background_blur = 0
 
 -- Tab Bar Settings
