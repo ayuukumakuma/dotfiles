b7c28fe72a9c730d49635c816306b500c3d1c3b0
ayuukumakuma <ayuu.kumakuma@gmail.com>
2026-04-15 17:28:17 +0900

    feat(nvim): add catppuccin colorscheme config

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -25,6 +25,8 @@ require("lazy").setup({
   spec = {
     { import = "plugins" },
   },
-  install = { colorscheme = { "habamax" } },
+  install = { colorscheme = { "catppuccin" } },
   checker = { enabled = true },
 })
+
+require("config.colorscheme")
