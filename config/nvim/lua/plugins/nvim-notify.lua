785af5cdfde322c26e612a194011020550d4488e
ayuukumakuma <ayuu.kumakuma@gmail.com>
2026-04-23 19:22:30 +0900

    feat(nvim): migrate picker and terminal workflow to snacks
    snacks.nvim を導入して picker・notifier・terminal を置き換える
    - Telescope 系と toggleterm、nvim-notify の spec を削除する
    - noice は cmdline と message UI に役割を絞る

--- config/nvim/lua/plugins/nvim-notify.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "rcarriga/nvim-notify",
-  opts = {
-    background_colour = "#eff1f5",
-    render = "compact",
-    timeout = 3000,
-    top_down = false,
-  },
-}
--- config/nvim/lua/plugins/smart-open.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "danielfalk/smart-open.nvim",
-  branch = "0.2.x",
-  dependencies = {
-    "nvim-telescope/telescope-fzy-native.nvim",
-  },
-}
