2485e98e495c70c6b167dbf13f85b30af7576d7d
luvpame <ayuu.kumakuma@gmail.com>
2026-04-30 07:34:22 +0900

    chore(nix): stop tracking local config

--- nix/local.nix
+++
@@ -1,7 +0,0 @@
-{
-  darwinConfigName = "luvpame-darwin";
-  userName = "luvpame";
-  homeDirectory = "/Users/luvpame";
-  dotfilesRoot = "/Users/luvpame/dev/github.com/luvpame/dotfiles";
-  profile = "private";
-}
