47fa4b86f69d5d4dcb863fab32b35b639ce61ed4
luvpame <ayuu.kumakuma@gmail.com>
2026-05-11 11:03:27 +0900

    fix(cage): allow Claude Code cache writes

---
+++ nix/local.nix
@@ -0,0 +1,7 @@
+{
+  darwinConfigName = "nasuno-darwin";
+  userName = "nasuno.ayumu";
+  homeDirectory = "/Users/nasuno.ayumu";
+  dotfilesRoot = "/Users/nasuno.ayumu/dev/github.com/luvpame/dotfiles";
+  profile = "work";
+}
